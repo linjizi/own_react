f36de584ca517f23430c0356f14b9fc9fe01d2c8
linjizi <256106365@qq.com>
2025-07-07 08:16:45 +0800

    write own createElement function

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-const element = {
-	type: "h1",
-	props: {
-		title: "foo",
-		children: "Hello",
-	},
-};
+function createTextElement(text) {
+	return {
+		type: "TEXT_ELEMENT",
+		props: {
+			nodeValue: text,
+			children: [],
+		},
+	};
+}
+
+function createElement(type, props, ...children) {
+	return {
+		type,
+		props: {
+			...props,
+			children: children.map((child) => (typeof child === "object" ? child : createTextElement(child))),
+		},
+	};
+}
+
+const ReactDidact = {
+  createElement,
+}
+
+// when babel transpiles the JSX it will use the function we define.
+/** @jsx Didact.createElement */
+const element = (
+  <div id="foo">
+    <a>bar</a>
+    <b />
+  </div>
+)
+
+// 上述jsx通过babel转译后
+// const element = ReactDidact.createElement("div", { id: "foo" }, ReactDidact.createElement("a", null, "bar"), ReactDidact.createElement("b"));
 
 const container = document.getElementById("root");
-
-const node = document.createElement(element.type);
-node["title"] = element.props.title;
-
-const text = document.createTextNode("");
-text["nodeValue"] = element.props.children;
-
-console.dir(text);
-
-node.appendChild(text);
-container.appendChild(node);
+ReactDOM.render(element, container);
