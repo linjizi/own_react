77a186e5804ea96f6dd29a1b81bcc897c93c96e6
linjizi <2561063565@qq.com>
2025-07-07 17:42:39 +0800

    finish performUnitOfWork

--- index.js
+++ index.js
@@ -27,15 +27,84 @@ function assignProps(dom, props) {
 	}
 }
 
-function render(element, container) {
-	const { children, ...props } = element.props;
+function createDom(fiber) {
+	const { dom, props } = fiber;
 	const dom = element.type === "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(element.type);
-	for (const child of children) {
-		render(child, dom);
-	}
 	assignProps(dom, props)
 
-	container.appendChild(dom);
+	return dom;
+}
+
+function render(element, container) {
+	nextUnitOfWork = {
+		dom: container,
+		props: {
+			children: [element],
+		},
+	}
+}
+
+let nextUnitOfWork = null
+
+function workLoop(deadline) {
+	let shouldYield = false
+	while (nextUnitOfWork && !shouldYield) {
+		nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+		/*
+			返回当前空闲期间剩余的估计毫秒数
+			若时间大于1ms，继续执行下一个工作单元
+			时间小于1ms，退出
+		*/
+		shouldYield = deadline.timeRemaining() < 1
+	}
+	// 下一次空闲时间执行
+	requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+
+function performUnitOfWork(fiber) {
+	if (!fiber.dom) {
+		fiber.dom = createDom(fiber)
+	}
+
+	if (fiber.parent) {
+		fiber.parent.dom.appendChild(fiber.dom)
+	}
+
+	const elements = fiber.props.children
+	let index = 0
+	let prevSibling = null
+
+	while (index < elements.length) {
+		const element = elements[index]
+
+		const newFiber = {
+			type: element.type,
+			props: element.props,
+			parent: fiber,
+			dom: null,
+		}
+		if (index === 0) {
+			fiber.child = newFiber
+		} else {
+			prevSibling.sibling = newFiber
+		}
+
+		prevSibling = newFiber
+		index++
+	}
+	if (fiber.child) {
+		return fiber.child
+	}
+	let nextFiber = fiber
+	while (nextFiber) {
+		if (nextFiber.sibling) {
+			return nextFiber.sibling
+		}
+		nextFiber = nextFiber.parent
+
+	}
 }
 
 const ReactDidact = {
